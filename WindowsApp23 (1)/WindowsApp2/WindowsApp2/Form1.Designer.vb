cda734c7b2825b497f0aa0df4273bd624b387069
rafiak <rafal.szczepaniak97@gmail.com>
2017-06-29 21:57:57 +0200

    Projekt licz¹cy i wywietlaj¹cy moc na oporniku w zale¿noci od wybranego napiêcia

---
+++ WindowsApp23 (1)/WindowsApp2/WindowsApp2/Form1.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(14, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "E"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(59, 23)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(59, 50)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 53)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(23, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Rw"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(59, 98)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Oblicz"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 145)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(134, 394)
+        Me.ListBox1.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(226, 71)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(25, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "      "
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(217, 23)
+        Me.TrackBar1.Maximum = 10000
+        Me.TrackBar1.Minimum = 100
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(503, 45)
+        Me.TrackBar1.TabIndex = 7
+        Me.TrackBar1.Value = 100
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(13, 82)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(67, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Ilość kroków"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(730, 612)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents Label5 As Label
+End Class
---
+++ WindowsApp23 (1)/WindowsApp2/WindowsApp2/Form1.vb
@@ -0,0 +1,202 @@
+﻿
+Public Class Form1
+    Dim Powers As New List(Of Double)
+    Dim externalResistance As New List(Of Double)
+    Dim graphics As Graphics
+    Dim good As Boolean = True
+    Private Sub fillLists(ByRef howManySteps As Integer)
+
+
+        Dim Rw, Ez, Rext, dR, P As Double
+        Powers.Clear()
+        externalResistance.Clear()
+        howManySteps = TrackBar1.Value
+        Try
+            Ez = Double.Parse(TextBox1.Text)
+            Rw = Double.Parse(TextBox2.Text)
+        Catch
+            MsgBox("Podałeś nieprawidłową wartość w jednym z pól.", MsgBoxStyle.OkCancel, "Uwaga")
+            good = False
+
+        End Try
+        If (Ez = 0 Or Rw = 0) Then
+            MsgBox("Jedno z pól ma wartość 0, tak nie może być! Popraw to!", MsgBoxStyle.OkCancel, "Uwaga")
+            good = False
+        End If
+        Rext = 0.1 * Rw
+        dR = 1.9 * Rw / howManySteps
+        For i As Integer = 0 To howManySteps
+            externalResistance.Add(Rext)
+            P = (Ez / (Rw + Rext)) ^ 2 * Rext
+            Powers.Add(P)
+            Rext = Rext + dR
+        Next
+    End Sub
+
+    Private Sub printListBox()
+        ListBox1.Items.Clear()
+        For i As Integer = 0 To Powers.Count - 1 Step 1
+            ListBox1.Items.Add("P " & Math.Round(Powers.Item(i), 3) & "   R " & externalResistance.Item(i))
+        Next
+    End Sub
+
+    Private Sub drawDiagram(ByVal howMany)
+        graphics.Clear(BackColor)
+        Dim p As New Pen(Color.Black, 2)
+        Dim Px1, Px2, Py1, Py2, p1, w1, p2, w2, position As Integer
+        Dim endDiagram, scala As Double
+        Px1 = 190
+        Px2 = 700
+        Py1 = 100
+        Py2 = 550
+        scala = (Py2 - Py1 - 5) / (Powers.Max - Powers.Min)
+        graphics.DrawLine(p, Px1, Py1, Px2, Py1)
+        graphics.DrawLine(p, Px1, Py1, Px1, Py2)
+        graphics.DrawLine(p, Px1, Py2, Px2, Py2)
+        graphics.DrawLine(p, Px2, Py1, Px2, Py2)
+        Dim elementWidth As Double = (Px2 - Px1) / howMany
+        p.Color = Color.Blue
+        endDiagram = Px1
+        For i As Integer = 0 To howMany - 1
+            p1 = Math.Round(endDiagram)
+            w1 = Py2 - Math.Round(Powers.Item(i) * scala) + position
+            endDiagram = endDiagram + elementWidth
+            p2 = Math.Round(endDiagram)
+            w2 = Py2 - Math.Round(Powers.Item(i + 1) * scala) + position
+            graphics.DrawLine(p, p1, w1, p2, w2)
+            If (w2 < Py1 + 3) Then
+                i = 0
+                endDiagram = Px1
+                graphics.Clear(BackColor)
+                p.Color = Color.Black
+                graphics.DrawLine(p, Px1, Py1, Px2, Py1)
+                graphics.DrawLine(p, Px1, Py1, Px1, Py2)
+                graphics.DrawLine(p, Px1, Py2, Px2, Py2)
+                graphics.DrawLine(p, Px2, Py1, Px2, Py2)
+                p.Color = Color.Blue
+                position = position + 1
+            End If
+            If (w2 > Py2) Then
+                i = 0
+                endDiagram = Px1
+                graphics.Clear(BackColor)
+                p.Color = Color.Black
+                graphics.DrawLine(p, Px1, Py1, Px2, Py1)
+                graphics.DrawLine(p, Px1, Py1, Px1, Py2)
+                graphics.DrawLine(p, Px1, Py2, Px2, Py2)
+                graphics.DrawLine(p, Px2, Py1, Px2, Py2)
+                p.Color = Color.Blue
+                position = position - 1
+            End If
+        Next
+        resistanceScale(Px1, Px2, Py2)
+        powerScale(Py2, Py1, Px1)
+    End Sub
+
+    Private Sub resistanceScale(x0, xk, y)
+        Dim a As Integer = 0
+        Dim p As New Pen(Color.Black, 2)
+        Dim font1 As New Font(Me.Font, FontStyle.Bold)
+        Dim b1 As New SolidBrush(Color.Black)
+        Dim pointLeftTop As PointF
+
+        graphics.DrawLine(p, x0, y + 5, xk, y + 5)
+        For i As Integer = x0 To xk Step Math.Round((xk - x0) / 22)
+            graphics.DrawLine(p, i, y + 5, i, y + 15)
+            If (a Mod 11 = 0) Then
+                graphics.DrawLine(p, i, y + 5, i, y + 25)
+                pointLeftTop.X = i - 10
+                pointLeftTop.Y = y + 25
+
+                graphics.DrawString(Math.Round(externalResistance.Item(Math.Round((a * (externalResistance.Count - 1)) / 22)), 2), font1, b1, pointLeftTop)
+            End If
+            a = a + 1
+        Next
+
+
+    End Sub
+    Private Function pValue(a As Integer) As String
+        If ((Powers.Min * (22 - a) / 22 + Powers.Max * a / 22) > 10) Then
+            Dim i As Integer = 0
+            Dim d As Double = (Powers.Min * (22 - a) / 22 + Powers.Max * a / 22)
+            While (d > 10)
+                d = d / 10
+                i = i + 1
+            End While
+            Return Math.Round(d, 1) & "E" & i
+        ElseIf ((Powers.Min * (22 - a) / 22 + Powers.Max * a / 22) < 1) Then
+            Dim i As Integer = 0
+            Dim d As Double = (Powers.Min * (22 - a) / 22 + Powers.Max * a / 22)
+            While (d < 1)
+                d = d * 10
+                i = i - 1
+
+            End While
+            Return Math.Round(d, 1) & "E" & i
+        Else
+            Return Math.Round((Powers.Min * (22 - a) / 22 + Powers.Max * a / 22))
+        End If
+    End Function
+    Private Sub powerScale(y2, y1, x)
+        Dim a As Integer = 0
+        Dim p As New Pen(Color.Black, 2)
+        Dim font As New Font(Me.Font, FontStyle.Bold)
+        Dim brush As New SolidBrush(Color.Red)
+        Dim point As PointF
+
+        graphics.DrawLine(p, x - 3, y1, x - 3, y2)
+        For i As Integer = y2 To y1 Step Math.Round((y1 - y2) / 22)
+            graphics.DrawLine(p, x - 3, i, x - 8, i)
+            If (a Mod 11 = 0) Then
+                graphics.DrawLine(p, x - 3, i, x - 15, i)
+                point.X = x - 45
+                point.Y = i
+
+                graphics.DrawString(pValue(a), font, brush, point)
+            End If
+            a = a + 1
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim q As Integer
+        fillLists(q)
+        If (good) Then
+            printListBox()
+            drawDiagram(q)
+            showMaximum()
+        Else
+            good = True
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        graphics = Me.CreateGraphics()
+    End Sub
+    Private Function findMax() As Double
+        Dim maximum As Double
+        maximum = Powers.First
+        For Each p As Double In Powers
+            If (maximum < p) Then
+                maximum = p
+            End If
+        Next
+        Return maximum
+    End Function
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+    Private Sub showMaximum()
+        Label3.Text = "Największa moc występuje dla rezystancji równej " & Math.Round(externalResistance.Item(Powers.LastIndexOf(findMax)), 3) & " i wynosi ona " & Math.Round(findMax(), 3)
+    End Sub
+
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        Label5.Text = "Ilość kroków " & TrackBar1.Value
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+End Class
